7cd734536efdee7c32927ffecd69ad283e4e670a
xandresariasx <128938922+xandresariasx@users.noreply.github.com>
2023-05-11 16:16:29 +0200

    Update and rename LoadMainP3Options.m to LoadMainOptions.m

--- LoadMainOptions.m
+++ LoadMainOptions.m
@@ -1,14 +1,13 @@
 
-function [UseNetworkMasks,DoDWI,CurateDWI,BiasCorrectDWI,AnnotateDWI,...
+function [DoDWI,CurateDWI,BiasCorrectDWI,AnnotateDWI,...
 GlobalRegisterDWI,LocalRegisterDWI,roiFactorDW,GetParDWI,GlobalDateRegisterDWI,...
 LocalDateRegisterDWI,roiFactorLDW,UseSegmentationForLocalRegDWI,DoT1W,...
 CurateT1W,BiasCorrectT1,AnnotateT1W,GlobalRegisterT1W,SpetialRegT1,LocalRegisterT1W,...
 roiFactorT1,IncludeArteryLR,GetT1,CorrectFA,WeightT1,AirTh,ScaleT1Ims,RefTiss,gammaT1,gammaM0,...
 solver,CorrectBiasBetweenSlices,AssumeT1s,GetConc,ContrastBrand,ScaleMo,AssumeT1sC,AssumeT1Artery,GetPar,...
 GlobalDateRegisterT1,LocalDateRegisterT1,roiFactorLT1,UseSegmentationForLocalRegT1,...
-DoT1WDWIReg,roiFactorLT1DW,RotateViewDW]=LoadMainP3Options(Options)
-
-UseNetworkMasks=Options.UseNetworkMasks;
+DoT1WDWIReg,roiFactorLT1DW,RotateViewDW]=LoadMainOptions(Options)
+% Function that loads user options    
     
 DoDWI=Options.DoDWI;
 CurateDWI=Options.CurateDWI;
@@ -50,8 +49,8 @@ GetConc=Options.GetConc;
 GetPar=Options.GetPar;
 GlobalDateRegisterT1=Options.GlobalDateRegisterT1;
 LocalDateRegisterT1=Options.LocalDateRegisterT1;
-    roiFactorLT1=Options.roiFactorLT1;                     % 4 
-    UseSegmentationForLocalRegT1=Options.UseSegmentationForLocalRegT1;     % 1
+    roiFactorLT1=Options.roiFactorLT1;                      
+    UseSegmentationForLocalRegT1=Options.UseSegmentationForLocalRegT1;     
 DoT1WDWIReg=Options.DoT1WDWIReg;
     roiFactorLT1DW=Options.roiFactorLT1DW;
     RotateViewDW=Options.RotateViewDW;    

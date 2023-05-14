c61b281a5565a95fdce458953142ec259e0a2c78
xandresariasx <128938922+xandresariasx@users.noreply.github.com>
2023-05-14 19:15:03 +0200

    Add files via upload

---
+++ ReadPatientFilesP3.m
@@ -0,0 +1,44 @@
+function [FilesFolderNames,Infos,SeriesDescription]=ReadPatientFilesP3(FolderBase)
+
+Folder=FolderBase;
+Files=AdjustDirVariable(dir(Folder));
+
+aux=[Files(:).isdir];
+Files(aux)=[];
+
+FileNames={Files(:).name};
+FileFolders={Files(:).folder};
+
+FilesFolderNames=cellfun(@(x,y) [x '\' y], FileFolders, FileNames, 'UniformOutput', false);
+
+Isdicoms=cellfun(@(x) isdicom(x), FilesFolderNames, 'UniformOutput', false);
+
+FilesFolderNames(~cell2mat(Isdicoms))=[];;
+
+Infos=cellfun(@dicominfo, FilesFolderNames, 'UniformOutput', false);
+
+try
+    SeriesDescription=cellfun(@(x) x.SeriesDescription,Infos, 'UniformOutput', false);
+catch
+    for I=1:numel(Infos)
+        try
+            SeriesDescription{I}=Infos{I}.SeriesDescription;
+        catch
+            SeriesDescription{I}='';
+        end    
+    end
+end
+
+
+
+function b=isdicom(x)
+
+try
+    dicominfo(x);
+    b=1;
+catch
+    b=0;
+end
+
+
+

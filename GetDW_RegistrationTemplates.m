47ecc95a3d1bdf1e624487ae656985c18d35766c
xandresariasx <128938922+xandresariasx@users.noreply.github.com>
2023-05-14 21:02:39 +0200

    Update and rename GetDW_RegistrationTemplatesP3.m to GetDW_RegistrationTemplates.m

---
+++ GetDW_RegistrationTemplates.m
@@ -0,0 +1,33 @@
+function [Templates,TemplateDate,LocalRegTemplate]=GetDW_RegistrationTemplates(Folder)
+
+Foldersb=AdjustDirVariable(dir(Folder));
+Foldersb=Foldersb([Foldersb(:).isdir]);
+aux=arrayfun(@(x) AdjustDirVariable(dir([x.folder '\' x.name])), Foldersb,'UniformOutput',false);  
+if isempty(aux)
+    Templates=[];TemplateDate=[];LocalRegTemplate=[];
+    return
+end
+Templates=cellfun(@SelectMinB, aux,'UniformOutput',false);
+aux2=sort({Foldersb(:).name});
+if isempty(res)
+    res=1;
+end
+TemplateDate=aux2{res};
+LocalRegTemplate=['b=' Templates{res} '_' TemplateDate(3:end)];
+
+
+
+
+function MinB=SelectMinB(Dir)
+
+aux2={Dir.name};
+aux3=strcmp(aux2,'Processed');
+aux2(aux3)=[];
+MinB=sort(aux2);
+MinB=MinB{1};
+
+    
+
+
+
+
